d7cd92c2b8b2685c1d28e09e36ff00fbccb67e95
Numa Gout <numagout@gmail.com>
2021-12-04 20:48:02 +0100

    Les cellules possèdent un taux de phéromone en attribut

--- Version 2/cell.py
+++ Version 2/cell.py
@@ -8,16 +8,24 @@ class Cell:
        Attribute :
          - x,y : Coordonnée du la cellule sur le plateau
          - agent : l'agent sur la cell ou None si aucun
-         - objet : A, B ou None si aucun"""
+         - objet : A, B ou None si aucun
+         - taux : le taux de phéromone sur la case"""
 
     def __init__(self, x, y, agent, objet):
         self.x = x
         self.y = y
         self.agent = agent
         self.objet = objet
+        self.taux = 0
 
     def set_agent(self, agent):
         self.agent = agent
 
     def set_objet(self, objet):
         self.objet = objet
+
+    def get_pheromone(self):
+        return self.taux
+
+    def set_pheromone(self, n):
+        self.taux = n
